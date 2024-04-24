06f10db60d8276949908a389aab5ac4cfc298f84
GhoniyaPooja <poojaghoniya123@gmail.com>
2024-04-24 09:57:35 -0400

    net worth

---
+++ demo/scratchpad.js
@@ -0,0 +1,12 @@
+var box1 = "Herry Potter: Prisoner of Azkaban";
+var box2 = 21;
+
+console.log(box1);
+console.log(box2);
+
+var name = "Pooja";
+var city = "Bloomfield";
+var state = "New Jersey";
+console.log("My name is " + name)
+console.log("I am from " + city)
+console.log("I am from " + state)
---
+++ exercises/mathscripts/net_worth.js
@@ -0,0 +1,4 @@
+var assets = 10000;
+var debts = 2000;
+var netWorth = assets - debts;
+console.log("Youe net worth is " + netWorth)
