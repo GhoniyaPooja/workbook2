ef979e52255d25af26772cbcf1da90a68f45d555
GhoniyaPooja <poojaghoniya123@gmail.com>
2024-04-25 10:36:47 -0400

    pay rules exercise under, exactly and over 40 hours

--- exercises/ifScripts/pay_rules.js
+++ exercises/ifScripts/pay_rules.js
@@ -1,9 +1,28 @@
-var payRate = 12.50;
-var hoursWorked = 20;
-pay = hoursWorked * payRate
 
-if (hoursWorked > 40) {
-    pay = hoursWorked * 1.5;
+
+var payRate = 17.30;
+var hoursWorked = 45;
+var pay;
+
+// under 40 hourse
+if (hoursWorked < 40)
+ {
+    pay = hoursWorked * payRate;
+}
+
+// exactly 40 hourse
+else if (hoursWorked == 40) {
+    pay = hoursWorked * payRate;
+}
+
+
+// over 40 hours
+else {
+    pay =(40 * payRate) + (payRate * 1.5 * (hoursWorked - 40));
 }
 console.log(pay);
+
+
+
+
 
