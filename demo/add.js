f51c47d30039d209b1af0ff48c27c70a8f8ea639
GhoniyaPooja <poojaghoniya123@gmail.com>
2024-04-29 10:08:13 -0400

    add two number using function

--- demo/add.js
+++ demo/add.js
@@ -1,5 +1,17 @@
-var numberOne = 20;
+"use strict";
+
+// var numberOne = 20;
+// var numberTwo = 5;
+// var result = numberOne + numberTwo;
+// console.log(result);
+
+
+
+function add() {
+    var numberOne = 20;
 var numberTwo = 5;
 var result = numberOne + numberTwo;
 console.log(result);
-
+    
+}
+add();
