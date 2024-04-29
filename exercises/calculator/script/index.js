56bb47f786acf32baa30e05a4112ffd6ff284e14
GhoniyaPooja <poojaghoniya123@gmail.com>
2024-04-29 11:58:26 -0400

    calculator add button

--- exercises/calculator/script/index.js
+++ exercises/calculator/script/index.js
@@ -1,20 +1,22 @@
 "use strict";
 
-function init(){
-const addBtn = document.getElementById("addBtn");
-const subtractBtn = document.getElementById("subtractionBtn");
-const multipltBtn = document.getElementById("multipltBtn");
-const divideBtn = document.getElementById("divideBtn");
-addBtn.onclick = addCalculation;
+
+function add() {
+    var numberOne = 20;
+var numberTwo = 5;
+var result = numberOne + numberTwo;
+console.log(result);
+    
 }
+// add();
+
+function init() {
+    var addButton = document.getElementById("addButton")
+    addButton.onclick = add;
+}
+
+// init();
 
 window.onload = init;
 
-function addCalculation(){
-    const number1 = document.getElementById("number1");
-    const number2 = document.getElementById("numbe21");
-    let result = Number(number1.value) + Number(number2);
-    let answerInput = document.getElementById("answerInput");
 
-    answerInput.value = answer;
-}
