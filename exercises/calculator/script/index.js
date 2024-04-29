64f86c74c1a2be5d4e1321d483f0150e592a1eb1
GhoniyaPooja <poojaghoniya123@gmail.com>
2024-04-29 14:26:54 -0400

    calculator

--- exercises/calculator/script/index.js
+++ exercises/calculator/script/index.js
@@ -1,22 +1,73 @@
 "use strict";
 
-
-function add() {
-    var numberOne = 20;
-var numberTwo = 5;
-var result = numberOne + numberTwo;
-console.log(result);
-    
-}
-// add();
-
-function init() {
-    var addButton = document.getElementById("addButton")
-    addButton.onclick = add;
-}
-
 // init();
 
 window.onload = init;
 
 
+
+function init() {
+    const addButton = document.getElementById("addButton");
+    addButton.onclick = add;
+    var sunButton = document.getElementById("sunButton");
+    sunButton.onclick = sub;
+    var mulButton = document.getElementById("mulButton");
+    mulButton.onclick = mul;
+    var divButton = document.getElementById("divButton");
+    divButton.onclick = div;
+}
+
+
+function add() {
+var numberOneInput =parseInt(document.getElementById("numberOneInput").value);
+var numberTwoInput = parseInt(document.getElementById("numberTwoInput").value);
+var result = numberOneInput + numberTwoInput;
+document.getElementById("answer").value = result;
+
+    
+}
+// ...........SUB............
+
+
+
+function sub() {
+    var numberOneInput =parseInt(document.getElementById("numberOneInput").value);
+    var numberTwoInput = parseInt(document.getElementById("numberTwoInput").value);
+    var result = numberOneInput - numberTwoInput;
+    document.getElementById("answer").value = result;
+    
+        
+    }
+
+
+    // .......MULTIPLY.........
+
+    
+    function mul() {
+        var numberOneInput =parseInt(document.getElementById("numberOneInput").value);
+        var numberTwoInput = parseInt(document.getElementById("numberTwoInput").value);
+        var result = numberOneInput * numberTwoInput;
+        document.getElementById("answer").value = result;
+        
+            
+        }
+    
+
+      
+        
+        function div() {
+            var numberOneInput =parseInt(document.getElementById("numberOneInput").value);
+            var numberTwoInput = parseInt(document.getElementById("numberTwoInput").value);
+            var result = numberOneInput / numberTwoInput;
+            document.getElementById("answer").value = result;
+            var message = document.getElementById("message");
+            let numberOne = Number(numberOneInput.value);
+            let numberTwo = Number(numberTwoInput.value);
+            if (isNaN(numberTwoInput)) {
+                message.innerHTML = "NAN";
+            }
+        
+            }
+        
+
+            
